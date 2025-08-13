f087d3056cc6068225bd7c4bbad95b78dc3a0744
Wh0-am1 <sidheeque4441234@gmail.com>
2025-08-13 20:28:46 +0530

    added close on outer Global Dialoge

--- src/components/GlobalDialogue.jsx
+++ src/components/GlobalDialogue.jsx
@@ -1,90 +1,91 @@
 import { Icon } from "@iconify/react";
 import {
-    Box,
-    Checkbox,
-    Dialog,
-    FormControlLabel,
-    FormGroup,
-    IconButton,
-    Stack,
-    Typography,
+  Box,
+  Checkbox,
+  Dialog,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Stack,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 
 export default function GlobalDialogue({ open, setOpen, Data }) {
-    const [Tempopen, settempOpen] = useState(true);
+  const [Tempopen, settempOpen] = useState(true);
 
-    const HandleClose = () => {
-        settempOpen(false);
-    };
+  const HandleClose = () => {
+    settempOpen(false);
+  };
 
-    const data = {
-        title: {
-            "Lunch Marking Hours": "2PM to 12AM",
-            "Evening Food Marking Hours": "10 AM to 3 PM",
+  const data = {
+    title: {
+      "Lunch Marking Hours": "2PM to 12AM",
+      "Evening Food Marking Hours": "10 AM to 3 PM",
+    },
+    body: [
+      { bodyTitle: "Lunch Tommorrow", checkBox: false },
+      { bodyTitle: "Food For Evenening", checkBox: true },
+    ],
+    close: true,
+  };
+  return (
+    <Dialog
+      onClose={HandleClose}
+      open={Tempopen}
+      sx={{
+        px: { xs: 5, md: 10 },
+      }}
+      slotProps={{
+        paper: {
+          sx: {
+            borderRadius: 4,
+            minWidth: { xs: "100%", lg: 900 },
+          },
         },
-        body: [
-            { bodyTitle: "Lunch Tommorrow", checkBox: false },
-            { bodyTitle: "Food For Evenening", checkBox: true },
-        ],
-        close: true,
-    };
-    return (
-        <Dialog
-            open={Tempopen}
-            sx={{
-                px: { xs: 5, md: 10 },
-            }}
-            slotProps={{
-                paper: {
-                    sx: {
-                        borderRadius: 4,
-                        minWidth: { xs: "100%", lg: 900 },
-                    },
-                },
-            }}
-        >
-            <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    gap: 5,
-                    bgcolor: "#f4f6f8",
-                    p: 2,
-                    borderBottom: "0.2px solid #e5e8eb",
-                }}
-            >
-                <Box>
-                    {Object.entries(data.title).map(([key, value], index) => (
-                        <Stack direction={"row"} gap={1} key={index}>
-                            <Typography>{`${key} :`}</Typography>
-                            <Typography>{value}</Typography>
-                        </Stack>
-                    ))}
-                </Box>
-                {data.close && (
-                    <IconButton onClick={HandleClose}>
-                        <Icon icon="carbon:close-filled" width="22" height="22" />
-                    </IconButton>
-                )}
-            </Box>
-            <Box
-                sx={{
-                    p: 2,
-                }}
-            >
-                <FormGroup>
-                    {data.body.map((e, index) => (
-                        <FormControlLabel
-                            key={index}
-                            control={<Checkbox disabled={e.checkBox} />}
-                            label={e.bodyTitle}
-                        />
-                    ))}
-                </FormGroup>
-            </Box>
-        </Dialog>
-    );
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 5,
+          bgcolor: "#f4f6f8",
+          p: 2,
+          borderBottom: "0.2px solid #e5e8eb",
+        }}
+      >
+        <Box>
+          {Object.entries(data.title).map(([key, value], index) => (
+            <Stack direction={"row"} gap={1} key={index}>
+              <Typography>{`${key} :`}</Typography>
+              <Typography>{value}</Typography>
+            </Stack>
+          ))}
+        </Box>
+        {data.close && (
+          <IconButton onClick={HandleClose}>
+            <Icon icon="carbon:close-filled" width="22" height="22" />
+          </IconButton>
+        )}
+      </Box>
+      <Box
+        sx={{
+          p: 2,
+        }}
+      >
+        <FormGroup>
+          {data.body.map((e, index) => (
+            <FormControlLabel
+              key={index}
+              control={<Checkbox disabled={e.checkBox} />}
+              label={e.bodyTitle}
+            />
+          ))}
+        </FormGroup>
+      </Box>
+    </Dialog>
+  );
 }
